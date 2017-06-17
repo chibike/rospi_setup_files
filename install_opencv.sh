0611aa53224c1d05b6d2632b1f256c01e7af0612
Chibuike Okpaluba <chibike@users.noreply.github.com>
2017-06-17 23:42:28 +0100

    Updated code

--- install_opencv.sh
+++ install_opencv.sh
@@ -8,9 +8,9 @@ sudo apt-get install libjpeg8-dev libtiff5-dev libjasper-dev libpng12-dev
 sudo apt-get install libavcodec-dev libavformat-dev libswscale-dev libv4l-dev
 sudo apt-get install libxvidcore-dev libx264-dev
 
-sudo apt-get install libgtk-3-dev
+sudo apt-get install libgtk2.0-dev
 sudo apt-get install libatlas-base-dev gfortran
-sudo apt-get install python2.7-dev python3.5-dev
+sudo apt-get install python2.7-dev python3-dev
 
 cd ~
 wget -O opencv.zip https://github.com/Itseez/opencv/archive/3.1.0.zip
@@ -19,42 +19,23 @@ unzip opencv.zip
 wget -O opencv_contrib.zip https://github.com/Itseez/opencv_contrib/archive/3.1.0.zip
 unzip opencv_contrib.zip
 
-cd ~
-wget https://bootstrap.pypa.io/get-pip.py
-sudo python get-pip.py
-
-sudo pip install virtualenv virtualenvwrapper
-sudo rm -rf ~/get-pip.py ~/.cache/pip
-
-echo -e "\n# virtualenv and virtualenvwrapper" >> ~/.bashrc
-echo "export WORKON_HOME=$HOME/.virtualenvs" >> ~/.bashrc
-echo "source /usr/local/bin/virtualenvwrapper.sh" >> ~/.bashrc
-
-source ~/.bashrc
-
-mkvirtualenv cv -p python2
-workon cv
-
 cd ~/opencv-3.1.0/
 mkdir build
 cd build
 cmake -D CMAKE_BUILD_TYPE=RELEASE \
-    -D CMAKE_INSTALL_PREFIX=/usr/local \
-    -D INSTALL_PYTHON_EXAMPLES=ON \
-    -D INSTALL_C_EXAMPLES=OFF \
-    -D OPENCV_EXTRA_MODULES_PATH=~/opencv_contrib-3.1.0/modules \
-    -D PYTHON_EXECUTABLE=~/.virtualenvs/cv/bin/python \
-    -D BUILD_EXAMPLES=ON ..
+   -D CMAKE_INSTALL_PREFIX=/usr/local \
+   -D INSTALL_PYTHON_EXAMPLES=ON \
+   -D OPENCV_EXTRA_MODULES_PATH=~/opencv_contrib-3.1.0/modules \
+   -D BUILD_EXAMPLES=ON ..
 
 make -j2
-make clean
-make
+
+#make clean
+#make
 
 sudo make install
 sudo ldconfig
 
-cd ~/.virtualenvs/cv/lib/python2.7/site-packages/
-ln -s /usr/local/lib/python2.7/site-packages/cv2.so cv2.so
-
-cd ~
-rm -rf opencv-3.1.0 opencv_contrib-3.1.0 opencv.zip opencv_contrib.zip
+cd ~/
+#rm -rf opencv-3.1.0 opencv_contrib-3.1.0 opencv.zip opencv_contrib.zip
+rm -rf opencv-3.1.0 opencv_contrib-3.1.0
